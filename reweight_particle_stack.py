b904797e0c573c06f38b46d591a8e570c124cc95
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2024-09-07 18:36:25 -0400

    Update reweight_particle_stack.py

--- reweight_particle_stack.py
+++ reweight_particle_stack.py
@@ -225,7 +225,7 @@ def reweight_starfile(euler,particle,rotlim1,rotlim2,tiltlim1,tiltlim2,psilim1,p
         #Get number of lines in tmpfile
         numLinesTemptFile=len(open(tmp,'r').readlines())
 	if debug is True:
-		print "Number of lines in tmpfile is numLines=%i" (numLinesTemptFile)
+		print "Number of lines in tmpfile is numLines=%i" (str(numLinesTemptFile))
         #Throw error if number to be removed is greater than number in group
         if remove>numLinesTemptFile:
             print 'Error: Number of particles to removed from euler angle range is greater than the number of particles in given group. Check tmpfile122.txt for number of particles that are in euler angle group.Exiting'
