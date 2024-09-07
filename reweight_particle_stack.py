41e550a6b614a38c5458f0d092e94b295c9090c9
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2024-09-07 14:55:27 -0400

    Update reweight_particle_stack.py

--- reweight_particle_stack.py
+++ reweight_particle_stack.py
@@ -289,7 +289,7 @@ def reweight_starfile(euler,particle,rotlim1,rotlim2,tiltlim1,tiltlim2,psilim1,p
             #remove_flag=checkInList('tmpfile122_222.txt',counter)
             remove_flag=0
 	    if debug is True:
-		print line
+		#print line
 		print counter
 		print counter-header_particle
             if (counter-header_particle) in badparticlelist:
